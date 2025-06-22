761e6da7f37eabced41d016d6b37cbf7e73b7f42
Taras Senko <taras.senko.99@gmail.com>
2025-06-22 21:54:01 +0300

    add pages

---
+++ pages/catalog/[id].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <h1>Товар ID: {{ route.params.id }}</h1>
+</template>
+
+<style scoped></style>
---
+++ pages/error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+</script>
+
+<template>
+  <div class="h-screen flex flex-col items-center justify-center text-center">
+    <h1 class="text-5xl font-bold">404 - Сторінку не знайдено</h1>
+    <p class="mt-4 text-gray-600">
+      Ми не змогли знайти цю сторінку. Можливо, вона була видалена або ви ввели неправильну адресу.
+    </p>
+    <NuxtLink to="/" class="mt-6 text-blue-500 underline">Повернутись на головну</NuxtLink>
+  </div>
+</template>
+
+<style scoped></style>
