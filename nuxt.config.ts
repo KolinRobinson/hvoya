383322306c7538515e960112541c564bd670eeef
Taras Senko <taras.senko.99@gmail.com>
2025-06-23 00:41:02 +0300

    fix for vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.API_BASE || '/proxy',
+      apiBase: '/api/proxy',
       apiTargetBase: 'https://teamchallenge-shop.onrender.com',
     },
   },
