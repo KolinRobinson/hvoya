6c358f4d95d9b200f3dcdab9d673780938ed6a5d
Taras Senko <taras.senko.99@gmail.com>
2025-06-25 21:57:00 +0300

    add basic input

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import { boolean } from 'zod'
-
 definePageMeta({
   breadcrumbs: 'Catalogue',
 })
 
 const isTestChecked = ref<boolean>(true)
+const input1Value = ref<string>('')
 </script>
 
 <template>
   <h1>catalog</h1>
   <ui-breadcrumbs></ui-breadcrumbs>
   <ui-checkbox v-model:checked="isTestChecked"></ui-checkbox>
+  <ui-input :input-value="input1Value" placeholder="aaaa" required type="text"></ui-input>
 </template>
 
 <style scoped></style>
