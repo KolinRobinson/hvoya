824bfdab38470fac17176c24c2febbb980ac7f23
Taras Senko <taras.senko.99@gmail.com>
2025-06-27 01:56:41 +0300

    add adaptive

--- components/home-page/hero.vue
+++ components/home-page/hero.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-<section class="w-full h-[calc(100lvh-4rem)] flex flex-col items-center justify-center gap-[2.5rem] bg-neutral-150 bg-[url(/images/hero_screen.png)] bg-cover">
-  <h1 class="text-white font-brand text-4xl mb-5">Timeless comfort, Crafted for now</h1>
-  <ui-button class="border border-white text-white min-w-25">Shop now</ui-button>
+<section class="w-full h-[calc(100lvh-4rem)] flex flex-col items-center justify-center bg-neutral-150 bg-[url(/images/hero_screen.png)] bg-cover px-2">
+  <h1 class="text-white font-brand text-4xl mb-5 text-center max-xl:text-[44px] max-md:text-[38px] max-md:mb-4 max-sm:text-xl max-sm:mb-3">Timeless comfort, Crafted for now</h1>
+  <ui-button class="border border-white text-white min-w-25 max-sm:min-w-14">Shop now</ui-button>
 
 </section>
 </template>
