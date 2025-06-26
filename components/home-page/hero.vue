ba768ca4fbf8a3b6e44ada1a01463049034bcfdc
Taras Senko <taras.senko.99@gmail.com>
2025-06-27 02:06:03 +0300

    update media query

--- components/home-page/hero.vue
+++ components/home-page/hero.vue
@@ -4,8 +4,8 @@
 
 <template>
 <section class="w-full h-[calc(100lvh-4rem)] flex flex-col items-center justify-center bg-neutral-150 bg-[url(/images/hero_screen.png)] bg-cover px-2">
-  <h1 class="text-white font-brand text-4xl mb-5 text-center max-xl:text-[44px] max-md:text-[38px] max-md:mb-4 max-sm:text-xl max-sm:mb-3">Timeless comfort, Crafted for now</h1>
-  <ui-button class="border border-white text-white min-w-25 max-sm:min-w-14">Shop now</ui-button>
+  <h1 class="text-white font-brand text-4xl mb-5 text-center max-xl:text-[44px] max-md:text-[38px] max-md:mb-4 max-sm:text-2xl max-xs:text-xl max-xs:mb-3">Timeless comfort, Crafted for now</h1>
+  <ui-button class="border border-white text-white min-w-25 max-xs:min-w-14">Shop now</ui-button>
 
 </section>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -115,10 +115,18 @@ export default {
     gap: {
       grid: '20px',
     },
+    screens: {
+      xs: '448px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     container: {
       center: true,
       padding: '20px',
       screens: {
+        xs: '448px',
         sm: '640px',
         md: '768px',
         lg: '1024px',
